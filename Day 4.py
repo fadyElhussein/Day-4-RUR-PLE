85c6ad155e370ddc7b798460dd8ec5521e8e4199
Husseiny <83413691+fadyElhussein@users.noreply.github.com>
2021-10-29 19:47:29 +0200

    RUR-PLE

---
+++ Day 4.py
@@ -0,0 +1,9 @@
+while not on_beeper():
+    if  right_is_clear():
+        repeat(turn_left,3)
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
+turn_off()
